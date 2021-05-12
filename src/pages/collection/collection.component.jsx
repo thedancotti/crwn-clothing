1b6e90dd05a482a3ee0768e6ff3b488345407a9d
Daniel-Sebastian Buhaianu <dsb.tech@192.168.1.3>
2021-05-12 19:58:26 +0300

    added redux persist, updated selectors and improved redux flow

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => {
+    console.log(collection);
+    return (
+        <div className="collection">
+            <h2>Collection PAGE</h2>
+        </div>
+    );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
