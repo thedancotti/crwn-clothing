452ae056c21ed0a12c785246681b445d76f2031f
Daniel-Sebastian Buhaianu <dsb.tech@192.168.1.3>
2021-05-12 20:43:31 +0300

    memoization, updated selectors and components related to shop and collection

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,13 +3,20 @@ import { connect } from 'react-redux';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-    console.log(collection);
+    const { title, items } = collection;
     return (
-        <div className="collection">
-            <h2>Collection PAGE</h2>
+        <div className="collection-page">
+            <h2 className="title">{ title }</h2>
+            <div className="items">
+                {items.map(item => 
+                    <CollectionItem key={item.id} item={item} />
+                )}
+            </div>
         </div>
     );
 };
